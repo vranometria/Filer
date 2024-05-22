ad3bdadf588bbd3165f99f243de0838f7d450a12
Administrator <administrator@example.com>
2024-05-22 22:59:26 +0900

    プロジェクト ファイルを追加します。

---
+++ Filer/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Filer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
